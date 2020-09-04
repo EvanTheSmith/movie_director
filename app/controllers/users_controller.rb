aae1f9b1edde83b20c27ec9941630f48a63bc7e2
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-04 06:35:46 -0500

    Sign up is working, added more styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      flash[:error] = @user.errors.full_message
+      flash[:error] = "Error! "+@user.errors.full_messages.join(', ')
       redirect_to root_path
     end
   end
 
   private 
   def user_params
-  params.require(:user).permit(:name, :password)
+  params.require(:user).permit(:username, :password)
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 root 'welcome#index'
+post '/' => 'users#create'
 get '/auth/facebook/callback' => 'sessions#create'
 end
