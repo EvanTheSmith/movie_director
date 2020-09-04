97d6916214d106fda2cc06aea5d02f7fa461e145
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-04 04:57:24 -0500

    Modified helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
 
     def current_user
-      @current_user ||= User.find(session[:user_id])
+      User.find(session[:user_id])
     end
 
-     def logged_in?
-       current_user != nil
-     end
+    def logged_in?
+      current_user != nil
+    end
 end
