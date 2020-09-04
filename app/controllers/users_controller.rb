85cc98d8a16787450833eecc3cd16dfd23d51d37
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-04 06:14:10 -0500

    Setting up basics of User Controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  # def new
+  # @user = User.new
+  # end
+    
+  def create # Sign Up A New User Using Local Sign Up Page
+  @user = User.new(user_params)
+    if @user && @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = @user.errors.full_message
+      redirect_to root_path
+    end
+  end
+
+  private 
+  def user_params
+  params.require(:user).permit(:name, :password)
+  end  
+end
