991b97e716b27e5ce6c996825992bd3c6c9158e8
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-06 04:53:17 -0500

    Refactored local signup action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,21 @@ class UsersController < ApplicationController
   def new # Local Sign Up Form
   end
     
-  def create # Sign Up A New User Using Local Sign Up Page : Post Path
-    if @fb = User.find_by(username: user_params[:username])
-      if @fb.fb_id
-       flash[:error] = "Error! Facebook login required for this user."
-       redirect_to login_path
-      end
-    else
+  def create # Sign Up A New User Using Local Sign Up Page - Post Path
+  @user = User.new(user_params)
+  @fb = User.find_by(username: user_params[:username])
 
-      @user = User.new(user_params)
-      if @user.save
-       session[:user_id] = @user.id
-       redirect_to root_path
-      else
-       flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
-       redirect_to signup_path
-      end
+    if @fb && @fb.fb_id
+     flash[:error] = "Error! Facebook login required for this user."
+     redirect_to login_path
+    elsif @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
+      redirect_to signup_path
     end
+
   end
 
   private 
