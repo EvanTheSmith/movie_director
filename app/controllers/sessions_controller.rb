ba9d23ab3ff5d8192d6d8bda49d6b4f2fd6264db
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-06 07:10:08 -0500

    User login needs a lot of work.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,37 +5,22 @@ class SessionsController < ApplicationController
 
   def create # Local Log In Post Path
     @user = User.new(user_params)
-    if @user.valid?
-          @user = User.find_by(username: user_params[:username])
+    if user_params[:username] && user_params[:password]
+          @user_find = User.find_by(username: user_params[:username])
           if @user && @user.authenticate(user_params[:password])
            session[:user_id] = @user.id
            redirect_to root_path
           else
+           @user.valid? if @user
            flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
            redirect_to login_path
           end
     else
+     @user.valid?
+     @user.errors.delete(:username) if User.find_by(username: @user.username)
      flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
      redirect_to login_path
     end
-
-    #if !@user
-    #  @user = User.new
-    #  @user.save
-    #  flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
-    #  redirect_to login_path
-    #elsif @user.fb_id
-    #  flash[:error] = "Error! Facebook login required for this user."
-    #  redirect_to login_path # Refuse local login for users who registered via Facebook
-    #else
-    #  if !user_params[:password].blank? && @user.authenticate(user_params[:password])
-    #    session[:user_id] = @user.id
-    #    redirect_to root_path
-    #  else
-    #    flash[:error] = "Error! "+@user.errors.full_messages.join('+ ')
-    #    redirect_to login_path
-    #  end
-    # end
   end
 
   private
