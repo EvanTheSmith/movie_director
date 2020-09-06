456f84dc98d4c46fa6a8772e0ba0afe220f18c90
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-06 06:31:58 -0500

    Further changed User validation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
 validates :username, presence: true, uniqueness: true
-validate :not_already_exist_via_fb
 has_secure_password
+validate :not_already_exist_via_fb
 
 def not_already_exist_via_fb
  @user = User.find_by(:username => username)
   if @user && @user.fb_id
    errors.delete(:username)
+   errors.delete(:password)
    errors.add(:user, "must login via Facebook.")
   end
 end
