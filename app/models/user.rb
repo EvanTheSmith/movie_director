156d81a58be98edda7f2925e4c23d4b41cce0215
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-06 06:36:30 -0500

    Renamed custom User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
 validates :username, presence: true, uniqueness: true
 has_secure_password
-validate :not_already_exist_via_fb
+validate :not_created_by_fb
 
-def not_already_exist_via_fb
- @user = User.find_by(:username => username)
-  if @user && @user.fb_id
-   errors.delete(:username)
-   errors.delete(:password)
-   errors.add(:user, "must login via Facebook.")
-  end
+def not_created_by_fb
+ user = User.find_by(:username => username)
+ if user && user.fb_id
+  errors.delete(:username)
+  errors.delete(:password)
+  errors.add(:user, "must login via Facebook.")
+ end
 end
 
 end
