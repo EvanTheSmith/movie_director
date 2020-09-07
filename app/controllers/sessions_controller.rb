d8f0800c45d64acf94ab8f6838dfc50cbbee555d
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-07 02:28:45 -0500

    Local user login now working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create # Local Log In Post Path
-    @user = User.new(user_params)
-    if user_params[:username] && user_params[:password]
-          @user_find = User.find_by(username: user_params[:username])
-          if @user && @user.authenticate(user_params[:password])
-           session[:user_id] = @user.id
-           redirect_to root_path
-          else
-           @user.valid? if @user
-           flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
-           redirect_to login_path
-          end
-    else
-     @user.valid?
-     @user.errors.delete(:username) if User.find_by(username: @user.username)
-     flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
-     redirect_to login_path
-    end
+  user = User.find_by(username: user_params[:username])
+   if user && user.authenticate(user_params[:password])
+    session[:user_id] = user.id
+    redirect_to root_path
+   else
+    new_user = User.new(user_params)
+    new_user.valid?
+    new_user.errors.delete(:username) if user # Clear "Username already taken" error message if user was found
+    flash[:error] = "Error! "+new_user.errors.full_messages.join(' + ')
+    redirect_to login_path
+   end
   end
 
   private
