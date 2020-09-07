88d0425a4bd3a06d883c679f787008c25970b9a1
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-07 02:36:15 -0500

    Reworked all sessions paths, methods and routes into UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,33 @@
 class UsersController < ApplicationController
-  def new # Local Sign Up Form
+  def new_signup
+  end
+
+  def new_login
   end
     
-  def create # Sign Up A New User Using Local Sign Up Page - Post Path
+  def create_signup # Local Signup Post Path
   @user = User.new(user_params)
-
     if @user.save 
-     session[:user_id] = @user.id
-     redirect_to root_path
+    session[:user_id] = @user.id
+    redirect_to root_path
     else
-     flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
-     redirect_to signup_path
+    flash[:error] = "Error! "+@user.errors.full_messages.join(' + ')
+    redirect_to signup_path
     end
+  end
 
+  def create_login # Local Log In Post Path
+    user = User.find_by(username: user_params[:username])
+     if user && user.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+     else
+      new_user = User.new(user_params)
+      new_user.valid?
+      new_user.errors.delete(:username) if user # Clear "Username already taken" error message if user was found
+      flash[:error] = "Error! "+new_user.errors.full_messages.join(' + ')
+      redirect_to login_path
+     end
   end
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 root 'welcome#index'
 
 # Local sign up routes
-get '/signup' => 'users#new'                        # Local Sign Up Form
-post '/signup' => 'users#create'                    # Local Signup Post Path
+get '/signup' => 'users#new_signup'                 # Local Sign Up Form
+post '/signup' => 'users#create_signup'             # Local Signup Post Path
 
 # Local log in routes
-get '/login' => 'sessions#new'                      # Log In Form
-post '/login' => 'sessions#create'                  # Log In Post Path (NOTE: same route as Facebook login)
+get '/login' => 'users#new_login'                   # Log In Form
+post '/login' => 'users#create_login'               # Log In Post Path (NOTE: same route as Facebook login)
 
 # Facebook log in routes
 get '/auth/facebook/callback' => 'facebook#create'  # Sign in from Facebook
