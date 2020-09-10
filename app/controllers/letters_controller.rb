cdab0d148ebc0fa5265a3ee97875c4d2daf9a051
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-10 06:00:45 -0500

    Setting up the BASICS of the Letter index page

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,7 +1,10 @@
 class LettersController < ApplicationController
     def index
+     @letters = Letter.all # this isn't done yet
     end
 
     def show
+     @penpal = Penpal.find(params[:penpal_id])
+     @letter = Letter.find(params[:id])
     end
 end
--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,4 +1,7 @@
 class Letter < ApplicationRecord
 belongs_to :user
 belongs_to :penpal
+
+scope :my_letters, -> { where(user: User.find_by_id(1)) } # This is not working lol
+
 end
