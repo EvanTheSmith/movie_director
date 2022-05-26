49769709418b93bb7fb945a6a49104acf3af6dce
EvanTheSmith <ProfessionalEmail@Zoho.com>
2022-05-25 20:49:43 -0500

    Successfully converted Sqlite database to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_09_10_092756) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "interests", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
